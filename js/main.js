6c3cb9ae490cc976b287c19335a49d1b429904d5
vikaputinceva <vikaputin2008@gmail.com>
2026-02-11 02:01:50 +0700

    fix update task

--- js/main.js
+++ js/main.js
@@ -68,13 +68,13 @@ Vue.component('kanban-column', {
 
 Vue.component('task-card', {
     template: `
-        <div>
+         <div v-if="!isEditing && !isReturningToWork">
                 <p class="task-title">{{ task.title }}</p>
                 <p class="task-description">{{ task.description }}</p>
                 <p class="task-deadline">Дэдлайн: {{ formattedDate(task.deadline) }}</p>
                 <p class="task-created">Создана: {{ formattedDate(task.created) }}</p>
                 <p class="task-updated">Последнее обновление{{ formattedDate(task.updatedAt) }}</p>
-                <button @click="editTask">Редактировать</button>
+                <button @click="startEditing">Редактировать</button>
                 <button @click="deleteTask">Удалить</button>
                 <button v-if="columnIndex !== 3" @click="moveTask(columnIndex + 1)">Переместить вперед</button>
                 <button v-if="columnIndex === 2" @click="moveTask(1)">Вернуть в работу</button>
@@ -86,8 +86,38 @@ Vue.component('task-card', {
         formattedDate(date) {
             return date ? new Intl.DateTimeFormat('ru-RU').format(new Date(date)) : ''
         },
-        editTask() {
-            this.$emit('edit-task', this.task)
+         startEditing() {
+            this.isEditing = true
+        },
+        saveEditing() {
+            this.$emit('edit-task', {
+                ...this.task,
+                title: this.editedTask.title,
+                description: this.editedTask.description,
+                deadline: this.editedTask.deadline,
+                updatedAt: new Date()
+            })
+            this.isEditing = false
+        },
+        cancelEditing() {
+            this.isEditing = false
+            this.editedTask = {
+                title: this.task.title,
+                description: this.task.description,
+                deadline: this.task.deadline
+            }
+        },
+        startReturningToWork() {
+            this.isReturningToWork = true
+        },
+        confirmReturnToWork() {
+            this.$emit('move-task', this.task, 1, this.returnReason)
+            this.isReturningToWork = false
+            this.returnReason = ''
+        },
+        cancelReturnToWork() {
+            this.isReturningToWork = false
+            this.returnReason = ''
         },
         deleteTask() {
             this.$emit('delete-task', this.task)
@@ -123,7 +153,7 @@ let app = new Vue({
         }
     },
     methods: {
-        moveTask(task, newColumnIndex) {
+        moveTask(task, newColumnIndex, returnReason) {
             const currentColumnIndex = this.columns.findIndex(column => column.tasks.includes(task))
             if(currentColumnIndex !== -1) {
                 this.columns[currentColumnIndex].tasks = this.columns[currentColumnIndex].tasks.filter(t => t.id !== task.id)
@@ -134,13 +164,23 @@ let app = new Vue({
             if (newColumnIndex === 3) {
                 task.isOverdue = task.deadline && new Date(task.deadline) < new Date()
             }
+             // Добавляем причину возврата, если она есть
+            if (returnReason) {
+                task.returnReason = returnReason
+            }
             this.columns[newColumnIndex].tasks.push(task)
         },
-        editTask(task) {
-            task.title = prompt('Введите новый заголовок: ', task.title) || task.title
-            task.description = prompt('Введите новое описание: ', task.description) || task.description
-            task.deadline = prompt('Введите новый дэдлайн (YYYY-MM-DD):', task.deadline) || task.deadline
-            task.updatedAt = new Date()
+        editTask(updatedTask, columnIndex) {
+            this.columns[columnIndex].tasks =
+                this.columns[columnIndex].tasks.map((task) => {
+                    if (task.id === updatedTask.id) {
+                        return {
+                            ...task,
+                            ...updatedTask
+                        }
+                    }
+                    return task
+                })
         },
         deleteTask(task) {
             const columnIndex = this.columns.findIndex(column => column.tasks.includes(task))
